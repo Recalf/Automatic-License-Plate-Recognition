df45cb1f52bb9989521479c447a7a9fdc4ef0ef5
Recalf <toutou.abbadi2@gmail.com>
2026-03-19 17:11:12 +0000

    Clarify OCR model version in stream_inference.py
    Added comment to clarify OCR model version.

--- stream_inference.py
+++ stream_inference.py
@@ -24,7 +24,7 @@ DB_TABLE =
 
 
 # OCR
-OCR_MODEL_NAME = "cct-s-v2-global-model"
+OCR_MODEL_NAME = "cct-s-v2-global-model" # xs version: cct-s-v2-global-model
 OCR_CONF = 0.75 # threshold
 MIN_OCR_CHARS_LEN = 5
 OCR_EVERY_FRAMES = 5 # we have 2 checks to do ocr, this first, then if size (with margin eps) bigger
