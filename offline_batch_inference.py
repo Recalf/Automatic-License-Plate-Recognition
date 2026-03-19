d0cd0dc4756d9214071d82f0317e9b3c32de8ff1
Recalf <toutou.abbadi2@gmail.com>
2026-03-19 16:57:35 +0000

    Update and rename offline_headless_inference.py to offline_batch_inference.py

--- offline_batch_inference.py
+++ offline_batch_inference.py
@@ -1,34 +1,34 @@
 from engine import run
 
 # CONFIG
-VID_IN = "test/vid3.mp4"
+VID_IN = "vid5.mp4"
 OUT_VIDEO = "out/offline_result.mp4"
 RESULT_IMAGES_ROOT = "result_images"
 
 # YOLO
 CONF = 0.25
-IMGSZ = 1280
+IMGSZ = 1920
 MODEL_WEIGHTS = "runs/detect/train9/weights/last.pt"
 TRACKER = "model/custom_bytetrackv2.yaml"
 
 # DB
-DB_HOST = "localhost"
+DB_HOST = 
 DB_PORT = 
 DB_USER = 
 DB_PASSWORD = 
-DB_NAME = "license_plate_db"
-DB_TABLE = "plates"
+DB_NAME = 
+DB_TABLE = 
+
+DB_POOL_SIZE = 8 # for offline speed, DB can get fast
 
 # OCR
-OCR_MODEL_NAME = "cct-s-v1-global-model"
+OCR_MODEL_NAME = "cct-s-v2-global-model"
+OCR_CONF = 0.75 # threshold
 MIN_OCR_CHARS_LEN = 5
-OCR_EVERY_FRAMES = 15  
-AREA_EPS_RATIO = 0.05  # n% bigger size margin to do ocr again
-MIN_PLATE_W = 55  # how many pixels to not do ocr
-MIN_PLATE_H = 20
-
-# for offline speed, DB can get fast
-DB_POOL_SIZE = 8
+OCR_EVERY_FRAMES = 5 # we have 2 checks to do ocr, this first, then if size (with margin eps) bigger
+AREA_EPS_RATIO = 0.03  # n% bigger size margin to do ocr again
+MIN_PLATE_W = 25  # size threshold for ocr
+MIN_PLATE_H = 25
 
 
 def main():
@@ -42,6 +42,7 @@ def main():
         ocr_model_name=OCR_MODEL_NAME,
         conf=CONF,
         imgsz=IMGSZ,
+        ocr_conf=OCR_CONF,
         min_ocr_chars_len=MIN_OCR_CHARS_LEN,
         ocr_every_frames=OCR_EVERY_FRAMES,
         area_eps_ratio=AREA_EPS_RATIO,
